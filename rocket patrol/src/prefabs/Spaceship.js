506d4b4ac4f837978bb8093364db98cddd07a473
jerrylin4real <80376737+jerrylin4real@users.noreply.github.com>
2021-04-13 04:51:18 +0800

    Add files via upload

--- rocket patrol/src/prefabs/Spaceship.js
+++ rocket patrol/src/prefabs/Spaceship.js
@@ -1,23 +1,20 @@
 // Spaceship prefab
 class Spaceship extends Phaser.GameObjects.Sprite {
-    constructor(scene, x, y, texture, frame, pointValue) {
+    constructor(scene, x, y, texture, frame) {
         super(scene, x, y, texture, frame);
-        scene.add.existing(this);   // add to existing scene
-        //this.points = pointValue;   // store pointValue
-        //this.moveSpeed = game.settings.spaceshipSpeed;         // pixels per frame
+        scene.add.existing(this);
     }
 
     update() {
-        // move spaceship left
-        this.x -= this.moveSpeed;
-        // wrap around from left edge to right edge
-        if(this.x <= 0 - this.width) {
+        this.x -= 2;
+
+        if(this.x < -this.width) {
             this.x = game.config.width;
         }
     }
+
     reset() {
         this.x = game.config.width + 50;
         this.alpha = 1;
     }
-
 }
